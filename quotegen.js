8d832b09110f716823fdc8fa2e6c92cdd032575b
Ivan Herselman <ivanherselman1@gmail.com>
2021-02-23 09:18:29 +0200

    first bit at the cherry

---
+++ quotegen.js
@@ -0,0 +1,10 @@
+let quotes = ['The Best Way To Get Started Is To Quit Talking And Begin Doing. – Walt Disney','The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty. \– Winston Churchill','Don\’t Let Yesterday Take Up Too Much Of Today. – Will Rogers']
+function newQuote(){
+
+    let quoteLength = quotes.length; 
+    let randomIndex = Math.floor(Math.random() * quoteLength);
+    document.getElementById('quotedisplay').innerHTML = quotes[randomIndex] ;
+
+}
+
+console.log(newQuote()) ; 
